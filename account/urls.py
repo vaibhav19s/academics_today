3af323bdbe6b3bfe36638fa8fd5b1d393b54a1b9
vaibhav19s <30555234+vaibhav19s@users.noreply.github.com>
2018-06-28 20:13:19 +0530

    Add files via upload

---
+++ account/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import *
+from account.views import mail
+from account.views import profile
+from account.views import setting
+
+
+urlpatterns =(
+    url(r'^profile$', profile.profile_page),
+    url(r'^update_user$', profile.update_user),
+    url(r'^inbox$', mail.mail_page),
+    url(r'^send_private_message$', mail.send_private_message),
+    url(r'^view_private_message$', mail.view_private_message),
+    url(r'^delete_private_message$', mail.delete_private_message),
+    url(r'^settings$', setting.settings_page),
+    url(r'^update_password$', setting.update_password)
+)
